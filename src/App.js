50a06bbe5313704916cc3df9b651c7d2b159834b
Krishna-Kiran-Souri <73020912+Krishna-Kiran-Souri@users.noreply.github.com>
2020-11-16 14:22:42 +0530

    Home Screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
 import Header from "./Header.js";
+import Home from "./Home";
 export default function App() {
   return (
     <div>
       <Header />
-      {/*Body*/}
+      <Home />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div>
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://m.media-amazon.com/images/G/01/digital/video/sonata/US_SVOD_NonPrime_Banner/f69c4124-8751-4646-b8de-14e68f14ff8e._UR3000,600_SX1500_FMjpg_.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
