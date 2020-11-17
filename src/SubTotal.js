1a17a7668c139f0adc01dbebdee38c0506aa85e3
Krishna-Kiran-Souri <73020912+Krishna-Kiran-Souri@users.noreply.github.com>
2020-11-17 14:22:13 +0530

    Subtotal and items are being displayed

--- src/SubTotal.js
+++ src/SubTotal.js
@@ -2,16 +2,29 @@ import React from "react";
 
 import "./SubTotal.css";
 
+import { useStateValue } from "./StateProvider";
 import CurrencyFormat from "react-currency-format";
 
 function SubTotal() {
+  const [{ basket }, dispatch] = useStateValue();
+  var value = 0;
+  if (basket.length === 0) {
+    value = 0;
+  } else {
+    for (let i = 0; i < basket.length; i++) {
+      value += parseFloat(basket[i].price);
+    }
+  }
+
+  console.log("this is a value" + value.toString());
   return (
     <div className="subtotal">
       <CurrencyFormat
+        value={value}
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 items):
+              Subtotal (<span>{basket.length}</span> items):
               <strong>{`${value}`}</strong>
             </p>
             <small className="subtotal__gift">
@@ -24,6 +37,7 @@ function SubTotal() {
         thousandSeparator={true}
         prefix={"$"}
       />
+      <p>{value}</p>
       <button>Proceed to checkout</button>
     </div>
   );
